45acd970f57ae5e0665ad556e16f7230ef912659
Nguyen Phuong Lam <phuonglam@v-takeuchi.vn>
2021-05-17 15:40:25 +0700

    Learn Selection and Filter

--- LearnRevitApi/Properties/AssemblyInfo.cs
+++ LearnRevitApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LearnRevitApi")]
+[assembly: AssemblyTitle("LearnSelectionFilter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LearnRevitApi")]
+[assembly: AssemblyCompany("VinaTakeuchi")]
+[assembly: AssemblyProduct("LearnSelectionFilter")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ LearnRevitApi/SelectionFilterRibbon.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSelectionFilter
+{
+    public class SelectionFilterRibbon : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+
+
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
